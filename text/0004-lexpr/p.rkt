c62b4490832fb74b477194672af8ab7ec073b673
Jay McCarthy <jay.mccarthy@gmail.com>
2019-08-27 16:38:59 -0400

    check

--- text/0004-lexpr/p.rkt
+++ text/0004-lexpr/p.rkt
@@ -1,7 +1,41 @@
 #lang racket/base
 (require racket/match
          racket/list
+         racket/set
+         syntax/parse/define
+         (for-syntax racket/base
+                     racket/syntax)
+         racket/generic
          srfi/14)
+
+#|
+
+Goal: Syntax with only one way to write things an AST.
+
+Problem: Lining up definitions or other things, like:
+let foo = 1
+a = 2
+Solution 1: Allow multiple |s?
+let | foo | 1
+|   a | 2
+Problem 1.1: Formatting is annoying to maintain and must be fixed
+up.
+Modification 1.2: Treat = (and =>?) as another kind of balancer,
+like | because it is common.
+
+Problem: Comments
+Solution 1: Don't have comments and insist on literal programming
+for prose, plus logging, tests ("Show, don't tell"), good names,
+specification, etc, because the compiler doesn't execute or analyze
+comments.
+Problem 1.a: What about commenting out code?
+Solution 1.a.1: (when false ...) is easy, but doesn't work for
+macros.
+Solution 1.a.2: `git diff` and the kill ring has the knowledge you
+want
+
+|#
+
 
 (struct loc (line col pos) #:transparent)
 (define (current-loc ip)
@@ -150,21 +184,21 @@
 
   #|
 
-   parse : mode x constraints x success-cont x fail-cont
+  parse : mode x constraints x success-cont x fail-cont
 
-   mode = Expression or Text (for '@' mode)
+  mode = Expression or Text (for '@' mode)
 
-   constraints = a stack of constraints that must be satisfied before
-   it is possible to parse an individual expression
+  constraints = a stack of constraints that must be satisfied before
+  it is possible to parse an individual expression
 
-   success-cont = Called if the constraints are met AND an iexpr is
-   parsed, called with the iexpr
+  success-cont = Called if the constraints are met AND an iexpr is
+  parsed, called with the iexpr
 
-   fail-cont = Called if the constraints are NOT met; no characters
-   are read
+  fail-cont = Called if the constraints are NOT met; no characters
+  are read
 
   |#
-  
+
   (define (parse-lexprs pre)
     (define a (parse-lexpr pre #t))
     (if a
@@ -173,6 +207,118 @@
 
   (parse-lexpr '() #f))
 
+(define-simple-macro (define-char c?)
+  #:with *c? (format-id #'c? "*~a" #'c)
+  (define *c? (λ (v) (and (char? v) (c? v)))))
+
+(struct set-complement (s)
+  #:methods gen:set
+  [(define/generic super-set-member? set-member?)
+   (define (set-member? st i)
+     (not (super-set-member? (set-complement-s st) i)))])
+(define (string->set s) (list->set (string->list s)))
+
+(define ((set-mem s) v) (set-member? s v))
+
+(define follower (set-union (string->set " .,'()[]<>") (set eof)))
+(define number-follower (set-remove follower #\.))
+(define number-leader (set-union (set #\-) (string->set "0123456789")))
+
+(define (#%dot-list x y)
+  (list* '#%dot x
+         (match y
+           [(cons '#%dot y) y]
+           [_ (list y)])))
+
+(define (read-lexpr ip)
+  (port-count-lines! ip)
+
+  (define (parse-error . state)
+    (error 'parse-error "~v => unexpected ~v: ~e" state (peek-char ip) (read-bytes 128 ip)))
+
+  (define (readc-while s)
+    (match (peek-char ip)
+      [(? (set-mem s)) (cons (read-char ip) (readc-while s))]
+      [_ '()]))
+  (define (readc-until s)
+    (readc-while (set-complement s)))
+  (define (reads-until pred)
+    (list->string (readc-until pred)))
+  (define (expectc x)
+    (define y (peek-char ip))
+    (if (equal? x y)
+      (read-char ip)
+      (parse-error 'expectc x)))
+
+  (define (leader)
+    (match (peek-char ip)
+      [(? (set-mem number-leader))
+       (string->number (reads-until number-follower))]
+      [#\' (read-char ip)
+       (list '#%quote (unit))]
+      [#\( (read-char ip)
+       (grouped)]
+      [(or #\< #\>)
+       (string->symbol (string (read-char ip)))]
+      [x
+       (define l (reads-until follower))
+       (when (equal? "" l)
+         (parse-error 'leader))
+       (string->symbol l)]))
+
+  (define (unit)
+    (define l (leader))
+    (match (peek-char ip)
+      [#\. (read-char ip)
+       (#%dot-list l (unit))]
+      [#\( (read-char ip)
+       (list '#%fun-app l (seq #\)))]
+      [#\[ (read-char ip)
+       (list '#%member l (seq #\]))]
+      [#\< (read-char ip)
+       (list '#%param l (seq #\>))]
+      [_ l]))
+
+  (define (seq endc)
+    (match (peek-char ip)
+      [(== endc) (read-char ip)
+       '()]
+      [_
+       (cons (unit) (seq-tail endc))]))
+  (define (seq-tail endc)
+    (match (peek-char ip)
+      [(== endc) (read-char ip)
+       '()]
+      [#\, (read-char ip)
+       (expectc #\space)
+       (cons (unit) (seq-tail endc))]))
+
+  (define (grouped)
+    (match (peek-char ip)
+      [#\) (read-char ip)
+       '()]
+      [_
+       (cons (unit) (grouped-tail))]))
+  (define (grouped-tail)
+    (match (peek-char ip)
+      [#\) (read-char ip)
+       '()]
+      [#\space (read-char ip)
+       (cons (unit) (grouped-tail))]))
+
+  (define (line)
+    (let loop ()
+      (cons
+       (unit)
+       (match (peek-char ip)
+         [(? eof-object?) '()]
+         [#\space (read-char ip)
+          (loop)]
+         [_
+          (error 'parse-error "~a: unexpected ~v: ~e" 'line (peek-char ip) (read-bytes 128 ip))]))))
+
+  (line))
+
 (module+ test
   (require racket/list
            racket/file
@@ -180,22 +326,30 @@
            racket/port
            racket/system
            racket/pretty
-           racket/runtime-path)
+           racket/runtime-path
+           syntax/location)
   (define (value->file f v)
     (with-output-to-file f (λ () (pretty-write v)) #:exists 'replace))
 
   (define-runtime-path actual.rktd "actual.rktd")
   (define-runtime-path expected.rktd "expected.rktd")
-  (define (parse-test label in expected)
-    (define actual (call-with-input-string in parse))
-    (unless (equal? actual expected)
-      (eprintf "Test failed: ~a\n" label)
-      (displayln in)
-      (value->file actual.rktd actual)
-      (value->file expected.rktd expected)
-      (system* (find-executable-path "diff") "-u"
-               actual.rktd expected.rktd)
-      (newline)))
+  (define (read-test label in expected)
+    (eprintf "Reading ~a\n" label)
+    (define actual (call-with-input-string in read-lexpr))
+    (cond
+      [(equal? actual expected)
+       (eprintf "...test passed\n")]
+      [else
+       (eprintf "...test failed:\n")
+       (displayln in)
+       (value->file actual.rktd actual)
+       (value->file expected.rktd expected)
+       (system* (find-executable-path "diff") "-u"
+                actual.rktd expected.rktd)
+       (newline)]))
+
+  (define-simple-macro (rt in e)
+    (read-test (quote-srcloc) in 'e))
 
   (define-runtime-path x-in "x.txt")
   (define-runtime-path x-out "x.rktd")
@@ -218,37 +372,8 @@
     (define-values (le-line lexpr more-line more) (extract-md-block "```lexpr" line l))
     (define-values (se-line sexpr after-line after) (extract-md-block "```sexpr" more-line more))
     (unless (and (string=? "" lexpr) (string=? "" sexpr))
-      (parse-test (format "extracted L~a" le-line) lexpr (with-input-from-string sexpr read))
+      (read-test (format "extracted L~a" le-line) lexpr (with-input-from-string sexpr read))
       (extract-tests after-line after)))
 
   (define-runtime-path md "../0004-lexpr.md")
   (extract-tests 0 (file->lines md)))
-
-#|
-
-   Goal: Syntax with only one way to write things an AST.
-
-   Problem: Lining up definitions or other things, like:
-      let foo = 1
-            a = 2
-   Solution 1: Allow multiple |s?
-      let | foo | 1
-          |   a | 2
-   Problem 1.1: Formatting is annoying to maintain and must be fixed
-   up.
-   Modification 1.2: Treat = (and =>?) as another kind of balancer,
-   like | because it is common.
-
-   Problem: Comments
-   Solution 1: Don't have comments and insist on literal programming
-   for prose, plus logging, tests ("Show, don't tell"), good names,
-   specification, etc, because the compiler doesn't execute or analyze
-   comments.
-   Problem 1.a: What about commenting out code?
-   Solution 1.a.1: (when false ...) is easy, but doesn't work for
-   macros.
-   Solution 1.a.2: `git diff` and the kill ring has the knowledge you
-   want
-
-
-|#
