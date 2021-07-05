01073946841b446aaa5c6f25054160d74b95da72
Matthew Flatt <mflatt@racket-lang.org>
2021-07-05 12:06:52 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+This is a Rhombus experiment with Shrubbery notation and DrRacket
+editor support.
---
+++ shrubbery/main.rkt
@@ -0,0 +1,26 @@
+#lang racket/base
+
+(module reader racket/base
+  (require syntax/strip-context
+           "parse.rkt")
+  
+  (provide (rename-out [shrubbery-read read]
+                       [shrubbery-read-syntax read-syntax])
+           get-info)
+
+    (define (shrubbery-read in)
+      (syntax->datum
+       (shrubbery-read-syntax #f in)))
+ 
+  (define (shrubbery-read-syntax src in)
+    (strip-context
+     #`(module anything racket/base
+         '#,(parse-all in))))
+ 
+  (define (get-info in mod line col pos)
+    (lambda (key default)
+      (case key
+        [(color-lexer)
+         (dynamic-require 'shrubbery/syntax-color
+                          'shrubbery-lexer)]
+        [else default]))))
