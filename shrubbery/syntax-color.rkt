c2cf2d3687003b28f2cd14554fe07b2dd54ee00f
Matthew Flatt <mflatt@racket-lang.org>
2021-09-28 06:23:51 -0600

    constraint coloring lexer to return a string or EOF
    It looks like `module-lexer` promises more than it should relative to
    `start-colorer`, but accomodate it for now.

--- shrubbery/syntax-color.rkt
+++ shrubbery/syntax-color.rkt
@@ -5,4 +5,13 @@
 (provide shrubbery-lexer)
 
 (define (shrubbery-lexer in pos status)
-  (lex/status in pos status racket-lexer/status))
+  (let-values ([(tok type paren start end backup status)
+                (lex/status in pos status racket-lexer/status)])
+    (define (to-string-or-eof tok)
+      (cond
+        [(eof-object? tok) tok]
+        [(string? tok) tok]
+        [(token? tok) (to-string-or-eof (token-e tok))]
+        [(syntax? tok) (to-string-or-eof (syntax-e tok))]
+        [else "other"]))
+    (values (to-string-or-eof tok) type paren start end backup status)))
