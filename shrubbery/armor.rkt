d6e3a7a9c60efc65aeb3c038ddcad1d76872d0ca
Matthew Flatt <mflatt@racket-lang.org>
2021-11-01 09:20:49 -0600

    fix armorer to pass all current tests

--- shrubbery/armor.rkt
+++ shrubbery/armor.rkt
@@ -9,21 +9,23 @@
 (struct group (col
                bar-line  ; line start for a `|`
                add-semi?
-               close?)
+               close?
+               in-at?)
   #:transparent)
 
 (define (make-parse-state gs
                           #:skip? [skip? #f]
                           #:prev [prev #f])
   (parse-state gs
-               #f
+               skip?
                prev))
 
 (define (make-group col
                     #:bar-line [bar-line #f]
                     #:add-semi? [add-semi? #t]
-                    #:close? [close? #t])
-  (group col bar-line add-semi? close?))
+                    #:close? [close? #t]
+                    #:in-at? [in-at? #f])
+  (group col bar-line add-semi? close? in-at?))
 
 (define (toggle-armor t e)
   (armor-region t
@@ -37,6 +39,7 @@
   (define end-line (if (= (send t paragraph-start-position end-start-line) end)
                        (sub1 end-start-line)
                        end-start-line))
+  (define skip-adjust-lines (make-hasheqv))
   (define col (get-current-tab t start))
   (define init (+ start col))
   (define gs (list (make-group col)))
@@ -59,8 +62,21 @@
          (define category (classify-position t pos))
          (case category
            [(whitespace comment continue-operator)
+            (when (and (eq? category 'comment)
+                       (pair? (parse-state-gs state))
+                       (group-in-at? (car (parse-state-gs state))))
+              ;; line comment => don't adjust next lines indentation
+              (hash-set! skip-adjust-lines (add1 (send t position-paragraph s)) #t))
             (loop e prev-end #f state inserts)]
-           [(closer)
+           [(group-comment)
+            (define next-pos (skip-whitespace t e end))
+            (loop next-pos prev-end #t state (if (and (not init?)
+                                                      (pair? (parse-state-gs state))
+                                                      (group-add-semi? (car (parse-state-gs state)))
+                                                      (not (eq? 'bar-operator (classify-position t next-pos))))
+                                                 (cons (cons prev-end 'semi) inserts)
+                                                 inserts))]
+           [(closer at-closer)
             (cond
               [(pair? (parse-state-gs state))
                (close-one)]
@@ -81,11 +97,12 @@
             (define g (and (pair? (parse-state-gs state))
                            (car (parse-state-gs state))))
             (define g-col (and (not (parse-state-skip? state))
+                               g
                                (group-col g)))
             (define (more-nested)
               (define use-category (cond
                                      [(parse-state-skip? state) (case category
-                                                                  [(opener) category]
+                                                                  [(opener at-opener) category]
                                                                   [else 'skipped])]
                                      [else category]))
               (case use-category
@@ -105,7 +122,7 @@
                        ;; assume already armored
                        (loop next-pos e #f state inserts)]
                       [else
-                       (define init-pos (skip-whitespace t next-pos end))
+                       (define init-pos (skip-whitespace t next-pos end #:and-own-line-group-comment next-pos))
                        (cond
                          [(init-pos . > . end)
                           (close-immediately init-pos)]
@@ -141,15 +158,17 @@
                                 (close-immediately init-pos)])]
                             [else
                              (start-group)])])])])]
-                [(opener)
+                [(opener at-opener)
                  (define next-pos (skip-whitespace t e end))
                  (define line (line-start t next-pos))
                  (loop next-pos e #t
                        (make-parse-state (list (make-group (+ (- next-pos line)
                                                               (line-delta t line))
                                                            #:add-semi? #f
-                                                           #:close? #f))
-                                         #:skip? (armor-opener? t s)
+                                                           #:close? #f
+                                                           #:in-at? (eq? use-category 'at-opener)))
+                                         #:skip? (or (eq? use-category 'at-opener)
+                                                     (armor-opener? t s))
                                          #:prev state)
                        inserts)]
                 [else
@@ -205,14 +224,17 @@
        (unless (line . > . end-line)
          (define s-pos (send t paragraph-start-position line))
          (when ((get-current-tab t s-pos) . >= . col)
-           (send t insert (make-string (string-length str) #\space) s-pos)
+           (unless (hash-ref skip-adjust-lines line #f)
+             (send t insert (make-string (string-length str) #\space) s-pos))
            (loop (add1 line)))))))
   (send t end-edit-sequence))
 
 (define (armor-opener? t pos)
   (equal? (send t get-text pos (add1 pos)) "«"))
 
-(define (skip-whitespace t pos end #:same-line? [same-line? #f])
+(define (skip-whitespace t pos end
+                         #:same-line? [same-line? #f]
+                         #:and-own-line-group-comment [and-own-line-group-comment #f])
   (cond
     [(pos . >= . end) pos]
     [else
@@ -220,12 +242,20 @@
      (case category
        [(continue-operator)
         (define-values (s e) (send t get-token-range pos))
-        (skip-whitespace t e end #:same-line? same-line?)]
+        (skip-whitespace t e end #:same-line? same-line? #:and-own-line-group-comment and-own-line-group-comment)]
        [(whitespace comment)
         (define-values (s e) (send t get-token-range pos))
         (cond
           [(or (not same-line?)
                (eqv? (line-start t s) (line-start t e)))
+           (skip-whitespace t e end #:same-line? same-line? #:and-own-line-group-comment and-own-line-group-comment)]
+          [else pos])]
+       [(group-comment)
+        (define-values (s e) (send t get-token-range pos))
+        (cond
+          [(and and-own-line-group-comment
+                (> (line-start t s) (line-start t and-own-line-group-comment))
+                (only-whitespace-between? t e (send t paragraph-end-position (send t position-paragraph e))))
            (skip-whitespace t e end #:same-line? same-line?)]
           [else pos])]
        [else pos])]))
