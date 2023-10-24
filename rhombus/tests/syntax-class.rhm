racket/rhombus

commit 053523c4d8d2b7475d8c0ec663bf6945b356a1f5

revise `pattern` shorthand
Now that we allow a block before alts, the `pattern` syntax class
clause is redundant and arguably disfavored.  Not only that, it also
ties the meaning of the corresponding (unquote) binding operator to
it.  I think it'd be better that the syntax class clause simply be
removed, and the (unquote) binding operator be changed into an actual
shorthand for an inline syntax class matching.

My experience is that inline syntax classes are quite useful for the
same reason complex patterns in `syntax/parse` are useful, but using
them unfortunately requires an unnaturally large amount of rightward
shift.  Moreover, supplying `open` with inline syntax classes is
common but, again, looks ugly.  Therefore I believe `pattern` can
serve a better purpose this way.

Interestingly, syntax class clause macros originally can supply
spliced patterns through `pattern`.  This use is kept through changing
the expected expansion to a block optionally followed by alts.

A remaining problem is that the handling of repetitions *above* the
quasiquote doesn't seem to cover attributes.  Although this is not
directly related to `pattern` per se, the issue may be apparent when
the `pattern` binding is used in a composite binding form.  Again, the
fix should be generalizable.

rhombus/private/pattern.rkt
@@ -1,6 +1,10 @@
 #lang racket/base
 (require (for-syntax racket/base
-                     syntax/parse/pre)
+                     syntax/parse/pre
+                     (only-in shrubbery/print
+                              shrubbery-syntax->string)
+                     "annotation-string.rkt"
+                     "srcloc.rkt")
          "binding.rkt"
          (only-in "unquote-binding-primitive.rkt"
                   pattern
@@ -16,29 +20,42 @@
                     pattern))
 
 ;; the `pattern` form for a binding context is here;
-;; the `pattern` form for `syntax_class` is in "syntax-class-clause-primitive.rkt";
 ;; the `pattern` form for `$` is in "unquote-binding-primitive.rkt"
 
 (define-binding-syntax pattern
   (binding-transformer
    (lambda (stx)
-     (define (expand s tail)
+     (define (expand s)
        (syntax-parse #`(group #%quotes
                               (quotes (group (op $)
                                              (parens ;; needs `#%parens` binding
                                               (group . #,s)))))
          [b::binding
-          (values #'b.parsed tail)]))
+          #:with b-form::binding-form #'b.parsed
+          (binding-form
+           #'pattern-infoer
+           #`[#,(annotation-string-from-pattern (shrubbery-syntax->string stx))
+              b-form.infoer-id b-form.data])]))
      (with-syntax ([pattern (syntax-parse stx
                               [(head . _)
                                (datum->syntax #'here 'pattern #'head #'head)])])
        (syntax-parse stx
-         [(_ (~and a (_::alts alt ...)) . tail)
-          (expand #'(pattern a)
-                   #'tail)]
-         [(_ (~and pat (_::quotes . _)) (~and b (_::block . _)))
-          (expand #'(pattern pat b)
-                  #'())]
-         [(_ (~and pat (_::quotes . _)) . tail)
-          (expand #'(pattern pat)
-                  #'tail)])))))
+         [(_ . tail)
+          (values (expand (relocate+reraw
+                           (respan stx)
+                           #'(pattern . tail)))
+                  #'())])))))
+
+(define-syntax (pattern-infoer stx)
+  (syntax-parse stx
+    [(_ up-static-infos [annot-str infoer-id data])
+     #:with impl::binding-impl #'(infoer-id up-static-infos data)
+     #:with info::binding-info #'impl.info
+     (binding-info #'annot-str
+                   #'info.name-id
+                   #'info.static-infos
+                   #'info.bind-infos
+                   #'info.matcher-id
+                   #'info.committer-id
+                   #'info.binder-id
+                   #'info.data)]))

rhombus/private/syntax-class.rkt
@@ -27,8 +27,8 @@
                       rhombus/namespace)
                      syntax_class))
 
-(module+ for-pattern-clause
-  (provide (for-syntax parse-pattern-clause)))
+(module+ for-anonymous-syntax-class
+  (provide (for-syntax parse-anonymous-syntax-class)))
 
 (define-name-root syntax_class
   #:fields
@@ -36,19 +36,19 @@
 
 (define-for-syntax (parse-syntax-class stx #:for-together? [for-together? #f])
   (syntax-parse stx
-    ;; just immediate-patterns shorthand
-    [(form-id class-name args::class-args (_::alts alt ...))
+    [(form-id class-name args::class-args
+              (_::alts alt ...))
      (build-syntax-class stx (syntax->list #'(alt ...)) null
                          #:define-class-id #'define-syntax
                          #:class/inline-name #'class-name
                          #:class-formals #'args.formals
                          #:class-arity #'args.arity
                          #:for-together? for-together?)]
-    ;; patterns within `pattern` or as shorthand
     [(form-id class-name args::class-args
               ;; syntax-class clauses are implemented in "syntax-class-clause-primitive.rkt"
               (_::block clause::syntax-class-clause ...)
-              (~optional (_::alts alt ...)))     
+              ;; patterns can be spliced by one of the macros
+              (~optional (_::alts alt ...)))
      (define parsed-clauses (syntax->list #'(clause.parsed ...)))
      (define-values (pattern-alts kind-kw class-desc fields-ht swap-root opaque?)
        (extract-clauses stx
@@ -99,13 +99,12 @@
 ;; returns a `rhombus-syntax-class`
 (define-for-syntax (parse-anonymous-syntax-class who orig-stx expected-kind name tail)
   (syntax-parse tail
-    ;; immediate-patterns shorthand
     [((_::alts alt ...))
      (build-syntax-class orig-stx (syntax->list #'(alt ...)) null
                          #:class/inline-name name
                          #:expected-kind expected-kind)]
-    ;; patterns within `pattern`
     [((_::block clause::syntax-class-clause ...)
+      ;; patterns can be spliced by one of the macros
       (~optional (_::alts alt ...)))
      (define parsed-clauses (syntax->list #'(clause.parsed ...)))
      (define-values (pattern-alts kind-kw class-desc fields-ht swap-root opaque?)
@@ -122,18 +121,6 @@
                          #:opaque? opaque?
                          #:expected-kind expected-kind)]
     [_ (raise-syntax-error who "bad syntax" orig-stx)]))
-
-;; returns a `rhombus-syntax-class`
-(define-for-syntax (parse-pattern-clause stx expected-kind)
-  (define alts
-    (syntax-parse stx
-      [(_ (_::alts alt ...))
-       (syntax->list #'(alt ...))]
-      [(_ (~and pat (_::quotes . _)) (~and b (_::block . _)))
-       (list #'(block (group pat b)))]
-      [(_ (~and pat (_::quotes . _)))
-       (list #'(block (group pat)))]))
-  (build-syntax-class stx alts null #:expected-kind expected-kind))
 
 ;; returns a `rhombus-syntax-class` if `define-syntax-id` is #f, otherwise
 ;; returns a list of definitions

rhombus/private/unquote-binding-primitive.rkt
@@ -28,7 +28,7 @@
          (submod "function-parse.rkt" for-call)
          (only-in "import.rkt" as open)
          (submod  "import.rkt" for-meta)
-         (submod "syntax-class.rkt" for-pattern-clause)
+         (submod "syntax-class.rkt" for-anonymous-syntax-class)
          "sequence-pattern.rkt")
 
 (provide (for-space rhombus/unquote_bind
@@ -178,17 +178,23 @@
 (define-unquote-binding-syntax pattern
   (unquote-binding-transformer
    (lambda (stx)
-     (define inline-id #f)
-     (define rsc (parse-pattern-clause stx (current-unquote-binding-kind)))
-     (values (if rsc
-                 (build-syntax-class-pattern stx
-                                             rsc
-                                             #'#f
-                                             (syntax-parse stx [(form-id . _) #'form-id])
-                                             #f
-                                             inline-id)
-                 #'#f)
-             #'()))))
+     (syntax-parse stx
+       [(form-id (~optional form1:identifier) . tail)
+        (define rsc
+          (parse-anonymous-syntax-class (syntax-e #'form-id)
+                                        stx
+                                        (current-unquote-binding-kind)
+                                        #f
+                                        #'tail))
+        (values (if rsc
+                    (build-syntax-class-pattern stx
+                                                rsc
+                                                #'#f
+                                                #'form-id
+                                                (attribute form1)
+                                                #f)
+                    #'#f)
+                #'())]))))
 
 (begin-for-syntax
   (struct open-attrib (sym bind-id var)))

commit e03b14065caff65c050d06f367a2e9428efa5cc6

fix generation of attributes in inline `pattern`s
I'm not sure what's the exact purpose of `bind-counter`, as the scopes
are already provided by the identifier.  It also doesn't appear to do
the right thing at all, as evidenced by the test cases.

Instead, symbolic names are kept distinct at each *depth* level of
`pattern`, which is required for the proper propagation of attributes
in nested `pattern`s.  Moreover, attributes in `open`ed syntax classes
can get their context from the `pattern` form id, and therefore the
implementation of binding `pattern` is changed to not alter its
context.

Also fix a bug accidentally introduced in 053523c that `open`s named
`pattern`s.

rhombus/private/attribute-name.rkt
@@ -1,10 +1,19 @@
 #lang racket/base
 
-(provide compose-attr-name)
+(provide compose-attr-name
+         inline-attr-depth
+         inline-form-id)
 
-(define (compose-attr-name match-id sym id bind-counter)
-  ;; when `match-id` is #f, then we're opening an inline syntax class, and
-  ;; we want to avoid clashes in symbolic names (as long as they have different scopes)
+(define (compose-attr-name match-id sym id)
+  ;; when `match-id` is #f, then we're dealing with inline `pattern`s
   (if match-id
       (datum->syntax match-id (string->symbol (format "~a.~a" (syntax-e match-id) sym)) id)
-      (datum->syntax id (string->symbol (format "inline~a.~a" bind-counter sym)) id)))
+      (datum->syntax (or id (inline-form-id))
+                     (string->symbol (format "~a.~a.~a" (syntax-e (inline-form-id)) (inline-attr-depth) sym))
+                     id)))
+
+;; handle nested inline `pattern`s
+(define inline-attr-depth (make-parameter #f))
+
+;; handle `open`ed syntax classes
+(define inline-form-id (make-parameter #f))

rhombus/private/pattern.rkt
@@ -3,18 +3,17 @@
                      syntax/parse/pre
                      (only-in shrubbery/print
                               shrubbery-syntax->string)
-                     "annotation-string.rkt"
-                     "srcloc.rkt")
+                     "annotation-string.rkt")
          "binding.rkt"
+         "unquote-binding.rkt"
+         (submod "unquote-binding-primitive.rkt" for-parse-pattern)
          (only-in "unquote-binding-primitive.rkt"
-                  pattern
                   #%parens)
          (only-in "quasiquote.rkt"
                   #%quotes)
          (only-in "dollar.rkt"
                   $)
-         "parse.rkt"
-         "parens.rkt")
+         "parse.rkt")
 
 (provide (for-space rhombus/bind
                     pattern))
@@ -22,29 +21,28 @@
 ;; the `pattern` form for a binding context is here;
 ;; the `pattern` form for `$` is in "unquote-binding-primitive.rkt"
 
+(define-unquote-binding-syntax bounce-to-pattern
+  (unquote-binding-transformer
+   (lambda (stx)
+     (syntax-parse stx
+       [(_ . tail)
+        (parse-pattern #'tail)]))))
+
 (define-binding-syntax pattern
   (binding-transformer
    (lambda (stx)
-     (define (expand s)
-       (syntax-parse #`(group #%quotes
-                              (quotes (group (op $)
-                                             (parens ;; needs `#%parens` binding
-                                              (group . #,s)))))
-         [b::binding
-          #:with b-form::binding-form #'b.parsed
-          (binding-form
-           #'pattern-infoer
-           #`[#,(annotation-string-from-pattern (shrubbery-syntax->string stx))
-              b-form.infoer-id b-form.data])]))
-     (with-syntax ([pattern (syntax-parse stx
-                              [(head . _)
-                               (datum->syntax #'here 'pattern #'head #'head)])])
-       (syntax-parse stx
-         [(_ . tail)
-          (values (expand (relocate+reraw
-                           (respan stx)
-                           #'(pattern . tail)))
-                  #'())])))))
+     (syntax-parse #`(group #%quotes
+                            (quotes (group (op $)
+                                           (parens ;; needs `#%parens` binding
+                                            (group bounce-to-pattern . #,stx)))))
+       [b::binding
+        #:with b-form::binding-form #'b.parsed
+        (values
+         (binding-form
+          #'pattern-infoer
+          #`[#,(annotation-string-from-pattern (shrubbery-syntax->string stx))
+             b-form.infoer-id b-form.data])
+         #'())]))))
 
 (define-syntax (pattern-infoer stx)
   (syntax-parse stx

rhombus/private/syntax-class.rkt
@@ -406,7 +406,10 @@
                  (map (lambda (var)
                         #`(#:attr
                            [#,(if keep-attr-id?
-                                  (pattern-variable-id var)
+                                  (or (pattern-variable-id var)
+                                      ;; fallback for `open`ed syntax classes
+                                      (datum->syntax (inline-form-id)
+                                                     (pattern-variable-sym var)))
                                   (pattern-variable-sym var))
                             #,(pattern-variable-depth var)]
                            (#,(pattern-variable-unpack* var)
@@ -423,30 +426,30 @@
 ;; converts a `pattern` clause for `syntax-case` into a pattern suitable
 ;; for directly inlinding into a larger pattern
 (define-for-syntax (syntax-class-body->inline patterns inline-name)
-  #`(~or #,@(for/list ([pattern (in-list patterns)])
-              (syntax-parse pattern
-                [(_ pat body ...)
-                 #`(~and pat
-                         #,@(let loop ([body (syntax->list #'(body ...))]
-                                       [bind-counter 0])
-                              (cond
-                                [(null? body) '()]
-                                [(eq? (syntax-e (car body)) '#:do)
-                                 (cons #`(~do . #,(cadr body))
-                                       (loop (cddr body) bind-counter))]
-                                [(eq? (syntax-e (car body)) '#:with)
-                                 (cons #`(~parse #,(cadr body) #,(caddr body))
-                                       (loop (cdddr body) bind-counter))]
-                                [(eq? (syntax-e (car body)) '#:when)
-                                 (cons #`(~fail #:unless #,(cadr body))
-                                       (loop (cddr body)
-                                             bind-counter))]
-                                [(eq? (syntax-e (car body)) '#:attr)
-                                 (with-syntax ([[name depth] (cadr body)])
-                                   (define id.name (compose-attr-name inline-name (syntax-e #'name) #'name bind-counter))
-                                   (cons #`(~bind ([#,id.name depth] #,(caddr body)))
-                                         (loop (cdddr body) (add1 bind-counter))))]
-                                [else (error "unhandled pattern body" body)])))]))))
+  (define (convert-body body)
+    (if (null? body)
+        '()
+        (case (syntax-e (car body))
+          [(#:do)
+           (cons #`(~do . #,(cadr body))
+                 (convert-body (cddr body)))]
+          [(#:with)
+           (cons #`(~post (~parse #,(cadr body) #,(caddr body)))
+                 (convert-body (cdddr body)))]
+          [(#:when)
+           (cons #`(~post (~fail #:unless #,(cadr body)))
+                 (convert-body (cddr body)))]
+          [(#:attr)
+           (with-syntax ([[name depth] (cadr body)])
+             (define id.name (compose-attr-name inline-name (syntax-e #'name) #'name))
+             (cons #`(~bind ([#,id.name depth] #,(caddr body)))
+                   (convert-body (cdddr body))))]
+          ;; should not reach
+          [else (raise-syntax-error #f "unhandled pattern body" patterns body)])))
+  #`(~or* #,@(for/list ([pattern (in-list patterns)])
+               (syntax-parse pattern
+                 [(_ pat body ...)
+                  #`(~and pat #,@(convert-body (syntax->list #'(body ...))))]))))
 
 ;; ----------------------------------------
 

rhombus/private/unquote-binding-primitive.rkt
@@ -40,6 +40,9 @@
                     #%literal
                     #%block))
 
+(module+ for-parse-pattern
+  (provide (for-syntax parse-pattern)))
+
 ;; `#%quotes` is implemented in "quasiquote.rkt" because it recurs as
 ;; nested quasiquote matching, `_` is in "quasiquote.rkt" so it can be
 ;; matched literally, and plain identifiers are implemented in
@@ -175,26 +178,32 @@
                    tail)])]))
    'none))
 
+(define-for-syntax (parse-pattern stx)
+  (syntax-parse stx
+    [(form-id (~optional form1:identifier) . tail)
+     (parameterize ([inline-attr-depth (cond
+                                         [(inline-attr-depth) => add1]
+                                         [else 0])]
+                    [inline-form-id #'form-id])
+       (define rsc
+         (parse-anonymous-syntax-class (syntax-e #'form-id)
+                                       stx
+                                       (current-unquote-binding-kind)
+                                       #f
+                                       #'tail))
+       (values (if rsc
+                   (build-syntax-class-pattern stx
+                                               rsc
+                                               #'#f
+                                               (and (not (attribute form1))
+                                                    #'form-id)
+                                               (attribute form1)
+                                               #f)
+                   #'#f)
+               #'()))]))
+
 (define-unquote-binding-syntax pattern
-  (unquote-binding-transformer
-   (lambda (stx)
-     (syntax-parse stx
-       [(form-id (~optional form1:identifier) . tail)
-        (define rsc
-          (parse-anonymous-syntax-class (syntax-e #'form-id)
-                                        stx
-                                        (current-unquote-binding-kind)
-                                        #f
-                                        #'tail))
-        (values (if rsc
-                    (build-syntax-class-pattern stx
-                                                rsc
-                                                #'#f
-                                                #'form-id
-                                                (attribute form1)
-                                                #f)
-                    #'#f)
-                #'())]))))
+  (unquote-binding-transformer parse-pattern))
 
 (begin-for-syntax
   (struct open-attrib (sym bind-id var)))
@@ -222,13 +231,12 @@
                                swap-root-to-id))
       (define-values (attribute-bindings attribute-vars)
         (for/lists (bindings descs) ([var (in-list vars)]
-                                     [temp-attr (in-list (generate-temporaries (map pattern-variable-sym vars)))]
-                                     [bind-counter (in-naturals)])
+                                     [temp-attr (in-list (generate-temporaries (map pattern-variable-sym vars)))])
           (define name (pattern-variable-sym var))
           (define id (pattern-variable-id var))
           (define depth (pattern-variable-depth var))
           (define unpack*-form (pattern-variable-unpack* var))
-          (define id-with-attr (compose-attr-name match-id name id bind-counter))
+          (define id-with-attr (compose-attr-name match-id name id))
           (values #`[#,temp-attr #,(cond
                                      [(eq? depth 'tail)
                                       ;; bridge from a primitive syntax class, where we don't want to convert to
@@ -302,7 +310,7 @@
               (open-attrib (syntax-e id) id var)))]))
       (define pack-depth 0)
       (define dotted-bind? (and sc (not (identifier? sc)) (rhombus-syntax-class-splicing? rsc)))
-      (define instance-id (or match-id (car (generate-temporaries '(inline)))))
+      (define instance-id (or match-id (car (generate-temporaries (list (inline-form-id))))))
       (define swap-to-root-var
         (for/first ([var (in-list attribute-vars)]
                     #:when (eq? (pattern-variable-sym var) swap-to-root))

rhombus/tests/syntax-class.rhm
@@ -772,6 +772,120 @@ block:
     matcher x
     ~prints_like '2'
 
+// check (not) `open`ing named `pattern`s
+check:
+  ~eval
+  match '1'
+  | '$(pattern any | '$x')': x
+  ~raises "cannot reference an identifier before its definition"
+
+check:
+  ~eval
+  match '1'
+  | (pattern any | '$x'): x
+  ~raises "cannot reference an identifier before its definition"
+
+// check generated attributes in inline `pattern`s
+block:
+  fun m(stx):
+    match stx
+    | (pattern
+       | '$_ $_': field one = 1; field two = 2
+       | '$_': field two = 1):
+        two
+  check:
+    m('foo')
+    ~is 1
+  check:
+    m('foo bar')
+    ~is 2
+
+block:
+  fun m(stx):
+    syntax_class Rhs:
+      kind ~sequence
+    | '= $_ ...'
+    match stx
+    | (pattern
+       | '$lhs ... $(rhs :: Rhs)'
+       | '$lhs ...: $(rhs :: Block)'):
+        '($lhs ..., $rhs)'
+  check:
+    m('foo bar = baz')
+    ~prints_like '(foo bar, = baz)'
+  check:
+    m('foo bar: baz')
+    ~prints_like '(foo bar, :« baz »)'
+
+// check handling of nested `pattern`s
+block:
+  fun m(stx):
+    match stx
+    | (pattern
+       | '$(pattern
+            | '$who'
+            | '$_ $who')'):
+        who
+  check:
+    m('foo')
+    ~prints_like 'foo'
+  check:
+    m('foo bar')
+    ~prints_like 'bar'
+
+block:
+  fun m(stx):
+    match stx
+    | (pattern
+       | '$(pattern
+            | '$who')'
+       | '$_ $who'):
+        who
+  check:
+    m('foo')
+    ~prints_like 'foo'
+  check:
+    m('foo bar')
+    ~prints_like 'bar'
+
+// check handling of `open`ed syntax classes
+block:
+  fun m(op):
+    syntax_class Header:
+      kind ~sequence
+    | '$lbind $name $rbind'
+    | '$name $rbind': field lbind = #false
+    match op
+    | (pattern:
+         kind ~group
+       | '$(_ :: Header: open): $_'
+       | '($(_ :: Header: open)): $_'):
+        '$(lbind || '') $name $rbind'
+  check:
+    m('x + y: add(x, y)')
+    ~prints_like 'x + y'
+  check:
+    m('(+ y): add(x, y)')
+    ~prints_like '+ y'
+
+block:
+  fun m(op):
+    syntax_class Header:
+      kind ~sequence
+    | '$lbind $name $rbind'
+    match op
+    | (pattern:
+         kind ~group
+       | '$(_ :: Header: open): $_'
+       | '($name $rbind): $_': field lbind = #false):
+        '$(lbind || '') $name $rbind'
+  check:
+    m('x + y: add(x, y)')
+    ~prints_like 'x + y'
+  check:
+    m('(+ y): add(x, y)')
+    ~prints_like '+ y'
+
 // infer ~multi mode
 check:
   syntax_class C