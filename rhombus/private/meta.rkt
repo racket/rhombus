0751c78b41ec9afd7257242e7c619b82d873846e
Wing Hei Chan <whmunkchan@outlook.com>
2023-12-14 12:13:08 +0800

    allow single-group body in `meta`
    This form is similar to `define-for-syntax` in Racket, but more
    general.  In particular, it avoids the extra indentation as compared
    to the block form.

--- rhombus/private/meta.rkt
+++ rhombus/private/meta.rkt
@@ -25,20 +25,17 @@
   (declaration-transformer
    (lambda (stx)
      (syntax-parse stx
-       #:datum-literals (block bridge op |.|)
        [(form-id (_::block form ...))
         #'((begin-for-syntax
              (rhombus-top form ...)))]
-       ;; since definition contexts overlap declaration contexts,
-       ;; we have to handle `bridge` here, too:
-       [(form-id (op |.|) bridge . tail)
-        #'((rhombus-definition (group bridge . tail)))]))))
+       [(form-id . tail)
+        #'((begin-for-syntax
+             (rhombus-top (group . tail))))]))))
 
 (define-for-syntax (make-bridge-definer space-sym)
   (definition-transformer
    (lambda (stx)
      (syntax-parse stx
-       #:datum-literals (block)
        [(form-id name-seq::dotted-operator-or-identifier-sequence (tag::block form ...))
         #:with name::dotted-operator-or-identifier #'name-seq
         #`(#,(build-syntax-definition/maybe-extension
