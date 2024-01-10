racket/rhombus

commit 98fb988b03d98c1d863b5112834f6b18f43b366d

fix term unpacking in some meta functions
Terms originated from Rhombus can have extra wrappers around them,
which can make Racket operations fail.

rhombus/private/class-meta.rkt
@@ -11,7 +11,8 @@
                    class-clause-extract
                    method-shape-extract))
          "call-result-key.rkt"
-         "realm.rkt")
+         "realm.rkt"
+         "pack.rkt")
 
 (provide (for-space rhombus/namespace
                     class_meta))
@@ -252,9 +253,13 @@
                              "unrecognized key symbol"
                              "symbol" key)]))
 
-(define (describe who id)
+(define (check-identifier who id)
   (unless (identifier? id)
-    (raise-argument-error* who rhombus-realm "Identifier" id))
+    (raise-argument-error* who rhombus-realm "Identifier" id)))
+
+(define (describe who id-in)
+  (define id (unpack-term/maybe id-in))
+  (check-identifier who id)
   (define desc (syntax-local-value* (in-class-desc-space id) class-desc-ref))
   (define idesc (and (not desc)
                      (syntax-local-value* (in-class-desc-space id) class-internal-desc-ref)))

rhombus/private/interface-meta.rkt
@@ -13,7 +13,8 @@
                    class-clause-extract
                    method-shape-extract))
          "call-result-key.rkt"
-         "realm.rkt")
+         "realm.rkt"
+         "pack.rkt")
 
 (provide (for-space rhombus/namespace
                     interface_meta))
@@ -100,9 +101,13 @@
                              "unrecognized key symbol"
                              "symbol" key)]))
 
-(define (describe who id)
+(define (check-identifier who id)
   (unless (identifier? id)
-    (raise-argument-error* who rhombus-realm "Identifier" id))
+    (raise-argument-error* who rhombus-realm "Identifier" id)))
+
+(define (describe who id-in)
+  (define id (unpack-term/maybe id-in))
+  (check-identifier who id)
   (define desc (syntax-local-value* (in-class-desc-space id) interface-desc-ref))
   (unless desc
     (raise-arguments-error* who rhombus-realm

rhombus/private/reducer-macro.rkt
@@ -121,8 +121,20 @@
     (raise-argument-error* who rhombus-realm "maybe(Identifier)" maybe-id)))
 
 (begin-for-syntax
-  (define/arity (reducer_meta.pack wrapper-id binds step-id maybe-break-id maybe-final-id finish-id static-infos data)
+  (define/arity (reducer_meta.pack wrapper-id-in
+                                   binds
+                                   step-id-in
+                                   maybe-break-id-in
+                                   maybe-final-id-in
+                                   finish-id-in
+                                   static-infos
+                                   data)
     #:static-infos ((#%call-result #,syntax-static-infos))
+    (define wrapper-id (unpack-term/maybe wrapper-id-in))
+    (define step-id (unpack-term/maybe step-id-in))
+    (define maybe-break-id (unpack-term/maybe maybe-break-id-in))
+    (define maybe-final-id (unpack-term/maybe maybe-final-id-in))
+    (define finish-id (unpack-term/maybe finish-id-in))
     (check-identifier who wrapper-id)
     (check-syntax who binds)
     (check-identifier who step-id)

rhombus/private/static-info-macro.rkt
@@ -124,7 +124,8 @@
       (wrap-static-info* (wrap-expression form) (pack who info)))
     (pack-term (relocate+reraw e #`(parsed #:rhombus/expr #,e))))
 
-  (define/arity (statinfo_meta.lookup form key)
+  (define/arity (statinfo_meta.lookup form key-in)
+    (define key (unpack-term/maybe key-in))
     (check-syntax who form)
     (check-identifier who key)
     (define si

rhombus/private/syntax-meta.rkt
@@ -175,19 +175,16 @@
     #:static-infos ((#%call-result #,syntax-static-infos))
     (transform-in stx))
 
-  (define/arity (syntax_meta.is_static id/op)
-    (define (fail)
-      (raise-argument-error* who rhombus-realm "Identifier || Operator" id/op))
-    (define s (unpack-term id/op #f #f))
-    (cond
-      [(identifier? s)
-       (is-static-context? s)]
-      [s
-       (syntax-parse s
-         #:datum-literals (op)
-         [(op id) (is-static-context? #'id)]
-         [_ (fail)])]
-      [else (fail)]))
+  (define/arity (syntax_meta.is_static id/op-in)
+    (define id/op (unpack-term/maybe id/op-in))
+    (define id
+      (syntax-parse id/op
+        #:datum-literals (op)
+        [(~or* id:identifier (op id)) #'id]
+        [_ (raise-argument-error* who rhombus-realm
+                                  "Identifier || Operator"
+                                  id/op)]))
+    (is-static-context? id))
 
   (define-annotation-syntax SyntaxPhase
     (identifier-annotation #'phase? #'())))

rhombus/tests/statinfo-macro.rhm
@@ -11,6 +11,12 @@ check:
   use_x
   ~is "yes"
 
+check:
+  expr.macro 'also_use_x':
+    statinfo_meta.lookup('sub.x', Syntax.make_group(['anything']))
+  also_use_x
+  ~is "yes"
+
 expr.macro 'two $(pre_expr :: expr_meta.Parsed) $()':
   def expr = expr_meta.parse_more(pre_expr)
   def si = statinfo_meta.lookup(expr, statinfo_meta.index_get_key)

rhombus/private/pack.rkt
@@ -30,6 +30,7 @@
 
 (provide pack-term
          unpack-term
+         unpack-term/maybe
          pack-group
          unpack-group
          pack-multi
@@ -123,6 +124,11 @@
            (fail))]
       [(list? r) (and who (cannot-coerce-list who r))]
       [else (datum->syntax at-stx r)])))
+
+;; For meta functions that take terms as arguments
+(define (unpack-term/maybe v)
+  (and (syntax? v)
+       (unpack-term v #f #f)))
 
 ;; "Packs" to a `group` form, but `r` starts with `group` already
 (define (pack-group r) r)

commit dc70ff3746b7e51ade58c63b6b10f2a49ebf71f6

fix error message in some meta functions
Introduced in 98fb988.

rhombus/private/class-meta.rkt
@@ -253,13 +253,14 @@
                              "unrecognized key symbol"
                              "symbol" key)]))
 
-(define (check-identifier who id)
+(define (unpack-identifier who id-in)
+  (define id (unpack-term/maybe id-in))
   (unless (identifier? id)
-    (raise-argument-error* who rhombus-realm "Identifier" id)))
+    (raise-argument-error* who rhombus-realm "Identifier" id-in))
+  id)
 
 (define (describe who id-in)
-  (define id (unpack-term/maybe id-in))
-  (check-identifier who id)
+  (define id (unpack-identifier who id-in))
   (define desc (syntax-local-value* (in-class-desc-space id) class-desc-ref))
   (define idesc (and (not desc)
                      (syntax-local-value* (in-class-desc-space id) class-internal-desc-ref)))

rhombus/private/interface-meta.rkt
@@ -101,13 +101,14 @@
                              "unrecognized key symbol"
                              "symbol" key)]))
 
-(define (check-identifier who id)
+(define (unpack-identifier who id-in)
+  (define id (unpack-term/maybe id-in))
   (unless (identifier? id)
-    (raise-argument-error* who rhombus-realm "Identifier" id)))
+    (raise-argument-error* who rhombus-realm "Identifier" id-in))
+  id)
 
 (define (describe who id-in)
-  (define id (unpack-term/maybe id-in))
-  (check-identifier who id)
+  (define id (unpack-identifier who id-in))
   (define desc (syntax-local-value* (in-class-desc-space id) interface-desc-ref))
   (unless desc
     (raise-arguments-error* who rhombus-realm

rhombus/private/reducer-macro.rkt
@@ -112,13 +112,15 @@
   (unless (syntax? s)
     (raise-argument-error* who rhombus-realm "Syntax" s)))
 
-(define-for-syntax (check-identifier who id)
+(define-for-syntax (unpack-identifier who id-in [annot-str "Identifier"])
+  (define id (unpack-term/maybe id-in))
   (unless (identifier? id)
-    (raise-argument-error* who rhombus-realm "Identifier" id)))
+    (raise-argument-error* who rhombus-realm annot-str id-in))
+  id)
 
-(define-for-syntax (check-maybe-identifier who maybe-id)
-  (unless (or (not maybe-id) (identifier? maybe-id))
-    (raise-argument-error* who rhombus-realm "maybe(Identifier)" maybe-id)))
+(define-for-syntax (unpack-maybe-identifier who maybe-id-in)
+  (and maybe-id-in
+       (unpack-identifier who maybe-id-in "maybe(Identifier)")))
 
 (begin-for-syntax
   (define/arity (reducer_meta.pack wrapper-id-in
@@ -130,17 +132,12 @@
                                    static-infos
                                    data)
     #:static-infos ((#%call-result #,syntax-static-infos))
-    (define wrapper-id (unpack-term/maybe wrapper-id-in))
-    (define step-id (unpack-term/maybe step-id-in))
-    (define maybe-break-id (unpack-term/maybe maybe-break-id-in))
-    (define maybe-final-id (unpack-term/maybe maybe-final-id-in))
-    (define finish-id (unpack-term/maybe finish-id-in))
-    (check-identifier who wrapper-id)
+    (define wrapper-id (unpack-identifier who wrapper-id-in))
     (check-syntax who binds)
-    (check-identifier who step-id)
-    (check-maybe-identifier who maybe-break-id)
-    (check-maybe-identifier who maybe-final-id)
-    (check-identifier who finish-id)
+    (define step-id (unpack-identifier who step-id-in))
+    (define maybe-break-id (unpack-maybe-identifier who maybe-break-id-in))
+    (define maybe-final-id (unpack-maybe-identifier who maybe-final-id-in))
+    (define finish-id (unpack-identifier who finish-id-in))
     (check-syntax who static-infos)
     (check-syntax who data)
     (define packed-binds

rhombus/private/static-info-macro.rkt
@@ -87,9 +87,11 @@
   (unless (syntax? s)
     (raise-argument-error* who rhombus-realm "Syntax" s)))
 
-(define-for-syntax (check-identifier who id)
+(define-for-syntax (unpack-identifier who id-in)
+  (define id (unpack-term/maybe id-in))
   (unless (identifier? id)
-    (raise-argument-error* who rhombus-realm "Identifier" id)))
+    (raise-argument-error* who rhombus-realm "Identifier" id-in))
+  id)
 
 (begin-for-syntax
   (define/arity (statinfo_meta.pack stx)
@@ -125,9 +127,8 @@
     (pack-term (relocate+reraw e #`(parsed #:rhombus/expr #,e))))
 
   (define/arity (statinfo_meta.lookup form key-in)
-    (define key (unpack-term/maybe key-in))
     (check-syntax who form)
-    (check-identifier who key)
+    (define key (unpack-identifier who key-in))
     (define si
       (extract-static-infos
        (syntax-parse (unpack-group form who #f)

rhombus/private/syntax-meta.rkt
@@ -175,15 +175,24 @@
     #:static-infos ((#%call-result #,syntax-static-infos))
     (transform-in stx))
 
-  (define/arity (syntax_meta.is_static id/op-in)
+  (define (unpack-identifier-or-operator who id/op-in)
     (define id/op (unpack-term/maybe id/op-in))
     (define id
-      (syntax-parse id/op
-        #:datum-literals (op)
-        [(~or* id:identifier (op id)) #'id]
-        [_ (raise-argument-error* who rhombus-realm
-                                  "Identifier || Operator"
-                                  id/op)]))
+      (cond
+        [(identifier? id/op) id/op]
+        [id/op (syntax-parse id/op
+                 #:datum-literals (op)
+                 [(op o) #'o]
+                 [_ #f])]
+        [else #f]))
+    (unless id
+      (raise-argument-error* who rhombus-realm
+                             "Identifier || Operator"
+                             id/op-in))
+    id)
+
+  (define/arity (syntax_meta.is_static id/op-in)
+    (define id (unpack-identifier-or-operator who id/op-in))
     (is-static-context? id))
 
   (define-annotation-syntax SyntaxPhase

rhombus/tests/statinfo-macro.rhm
@@ -13,7 +13,7 @@ check:
 
 check:
   expr.macro 'also_use_x':
-    statinfo_meta.lookup('sub.x', Syntax.make_group(['anything']))
+    statinfo_meta.lookup('sub.x', Syntax.literal_group 'anything')
   also_use_x
   ~is "yes"
 