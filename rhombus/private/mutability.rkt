c8c6ff123cdd7e22d7f96597155992bddb054b1e
Wing Hei Chan <whmunkchan@outlook.com>
2024-01-25 12:45:58 +0800

    use `racket/mutability` under a version guard

--- rhombus/private/mutability.rkt
+++ rhombus/private/mutability.rkt
@@ -1,4 +1,5 @@
 #lang racket/base
+(require "version-case.rkt")
 
 (provide immutable-string?
          mutable-string?
@@ -11,20 +12,22 @@
          immutable-box?
          mutable-box?)
 
-;; TODO: use `racket/mutability` at the point where it makes sense to
-;; depend on a newer Racket
+;; TEMP approximate `racket/mutability`
+(meta-if-version-at-least
+ "8.9.0.3"
+ (require racket/mutability)
+ (begin
+   (define (immutable-string? v) (and (string? v) (immutable? v)))
+   (define (mutable-string? v) (and (string? v) (not (immutable? v))))
 
-(define (immutable-string? v) (and (string? v) (immutable? v)))
-(define (mutable-string? v) (and (string? v) (not (immutable? v))))
+   (define (immutable-bytes? v) (and (bytes? v) (immutable? v)))
+   (define (mutable-bytes? v) (and (bytes? v) (not (immutable? v))))
 
-(define (immutable-bytes? v) (and (bytes? v) (immutable? v)))
-(define (mutable-bytes? v) (and (bytes? v) (not (immutable? v))))
+   (define (immutable-vector? v) (and (vector? v) (immutable? v)))
+   (define (mutable-vector? v) (and (vector? v) (not (immutable? v))))
 
-(define (immutable-vector? v) (and (vector? v) (immutable? v)))
-(define (mutable-vector? v) (and (vector? v) (not (immutable? v))))
+   (define (immutable-hash? v) (and (hash? v) (immutable? v)))
+   (define (mutable-hash? v) (and (hash? v) (not (immutable? v))))
 
-(define (immutable-hash? v) (and (hash? v) (immutable? v)))
-(define (mutable-hash? v) (and (hash? v) (not (immutable? v))))
-
-(define (immutable-box? v) (and (box? v) (immutable? v)))
-(define (mutable-box? v) (and (box? v) (not (immutable? v))))
+   (define (immutable-box? v) (and (box? v) (immutable? v)))
+   (define (mutable-box? v) (and (box? v) (not (immutable? v))))))
