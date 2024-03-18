acd118a7966623758fce7bdd196506ce1c1590e3
Wing Hei Chan <whmunkchan@outlook.com>
2024-03-18 11:11:39 +0800

    fix negative integer in suffix `..`
    Suffix `..` should take an arbitrary integer as the left-hand side,
    but `in-naturals` only accepts nonnegative integers.  Reimplement an
    infinite range sequence for this.

--- rhombus/private/range.rkt
+++ rhombus/private/range.rkt
@@ -1,30 +1,29 @@
 #lang racket/base
 (require (for-syntax racket/base
-                     syntax/parse/pre
-                     "srcloc.rkt")
+                     syntax/parse/pre)
          "expression.rkt"
          "static-info.rkt"
          "parse.rkt"
          (prefix-in rhombus-a: "arithmetic.rkt")
          "sequence-constructor-key.rkt"
          "treelist.rkt"
-         (submod "list.rkt" for-listable))
+         (submod "list.rkt" for-listable)
+         "realm.rkt")
 
 (provide ..)
 
 (define-syntax ..
   (expression-infix-operator
    (expr-quote ..)
-   (list
-    (cons (expr-quote rhombus-a:+) 'weaker)
-    (cons (expr-quote rhombus-a:-) 'weaker)
-    (cons (expr-quote rhombus-a:*) 'weaker)
-    (cons (expr-quote rhombus-a:/) 'weaker))
+   `((,(expr-quote rhombus-a:+) . weaker)
+     (,(expr-quote rhombus-a:-) . weaker)
+     (,(expr-quote rhombus-a:*) . weaker)
+     (,(expr-quote rhombus-a:/) . weaker))
    'macro
    (lambda (form1 tail)
      (syntax-parse tail
        [(_)
-        (values (wrap-as-static-sequence #`(in-naturals #,form1))
+        (values (wrap-as-static-sequence #`(in-infinite-range #,form1))
                 #'())]
        [(_ . more)
         #:with (~var rhs (:infix-op+expression+tail #'..)) #'(group . more)
@@ -51,3 +50,42 @@
 
 (define (in-listable-range/proc a b)
   (listable-range (in-range a b)))
+
+(define (check-integer who int)
+  (unless (exact-integer? int)
+    (raise-argument-error* who rhombus-realm "Int" int)))
+
+(define-sequence-syntax in-infinite-range
+  (lambda () #'in-infinite-range/proc)
+  (lambda (stx)
+    (syntax-parse stx
+      [[(id) (_ start-expr)]
+       #'[(id)
+          (:do-in
+           ([(start) start-expr])
+           (unless (variable-reference-from-unsafe? (#%variable-reference))
+             (check-integer 'in-infinite-range start))
+           ([pos start])
+           #t
+           ([(id) pos])
+           #t
+           #t
+           ((+ pos 1)))]]
+      [_ #f])))
+
+(define (in-infinite-range/proc start)
+  (check-integer 'in-infinite-range start)
+  (infinite-range start))
+
+(struct infinite-range (start)
+  #:property prop:sequence (lambda (r)
+                             (make-do-sequence
+                              (lambda ()
+                                (values
+                                 values
+                                 #f
+                                 add1
+                                 (infinite-range-start r)
+                                 #f
+                                 #f
+                                 #f)))))
