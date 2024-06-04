6b0b6656c007ece4b7815927cbc9c81288ac6959
Matthew Flatt <mflatt@racket-lang.org>
2024-06-03 18:22:18 -0600

    reduce excessive transitive require of "list.rkt"

--- rhombus/private/list.rkt
+++ rhombus/private/list.rkt
@@ -6,6 +6,7 @@
          "treelist.rkt"
          (submod "treelist.rkt" unsafe)
          "mutable-treelist.rkt"
+         "to-list.rkt"
          "provide.rkt"
          "composite.rkt"
          "expression.rkt"
@@ -1386,44 +1387,6 @@
            l)
       (and (pair? l)
            (maybe-list-tail (cdr l) (sub1 n)))))
-
-(define-values (prop:Listable Listable? Listable-ref)
-  (make-struct-type-property 'Listable))
-
-(define (listable? v)
-  (or (treelist? v)
-      (list? v)
-      (vector? v)
-      (mutable-treelist? v)
-      (Listable? v)))
-
-(define (to-treelist who v)
-  (cond
-    [(treelist? v) v]
-    [(list? v) (list->treelist v)]
-    [(vector? v) (vector->treelist v)]
-    [(mutable-treelist? v) (mutable-treelist-snapshot v)]
-    [else (general-to-treelist who v)]))
-
-(define (to-list who v)
-  (cond
-    [(treelist? v) (treelist->list v)]
-    [(list? v) v]
-    [(vector? v) (vector->list v)]
-    [(mutable-treelist? v) (mutable-treelist->list v)]
-    [(general-to-treelist who v) => treelist->list]
-    [else #f]))
-
-(define (general-to-treelist who v)
-  (define methods (Listable-ref v #f))
-  (cond
-    [(not methods)
-     (and who
-          (raise-argument-error* who rhombus-realm "Listable" v))]
-    [else
-     (define lst ((vector-ref methods 0) v))
-     ;; guarded by method result
-     lst]))
 
 (define (ensure-treelist v)
   (or (to-treelist #f v)
--- rhombus/private/to-list.rkt
+++ rhombus/private/to-list.rkt
@@ -1,11 +1,52 @@
 #lang racket/base
 (require "treelist.rkt"
-         (submod "list.rkt" for-listable))
+         "mutable-treelist.rkt"
+         "realm.rkt")
 
-(provide listable?
+(provide prop:Listable Listable? Listable-ref
+
+         listable?
          to-list
          to-treelist
          maybe-list->treelist)
+
+(define-values (prop:Listable Listable? Listable-ref)
+  (make-struct-type-property 'Listable))
+
+(define (listable? v)
+  (or (treelist? v)
+      (list? v)
+      (vector? v)
+      (mutable-treelist? v)
+      (Listable? v)))
+
+(define (to-list who v)
+  (cond
+    [(treelist? v) (treelist->list v)]
+    [(list? v) v]
+    [(vector? v) (vector->list v)]
+    [(mutable-treelist? v) (mutable-treelist->list v)]
+    [(general-to-treelist who v) => treelist->list]
+    [else #f]))
+
+(define (to-treelist who v)
+  (cond
+    [(treelist? v) v]
+    [(list? v) (list->treelist v)]
+    [(vector? v) (vector->treelist v)]
+    [(mutable-treelist? v) (mutable-treelist-snapshot v)]
+    [else (general-to-treelist who v)]))
+
+(define (general-to-treelist who v)
+  (define methods (Listable-ref v #f))
+  (cond
+    [(not methods)
+     (and who
+          (raise-argument-error* who rhombus-realm "Listable" v))]
+    [else
+     (define lst ((vector-ref methods 0) v))
+     ;; guarded by method result
+     lst]))
 
 (define (maybe-list->treelist v)
   (cond
