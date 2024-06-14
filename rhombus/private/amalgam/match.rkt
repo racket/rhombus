64195f914e062f15a8ef4b8c7ad1902c1b87c72a
Wing Hei Chan <whmunkchan@outlook.com>
2024-06-14 16:28:55 +0800

    refactor `match` expansion

--- rhombus/private/amalgam/match.rkt
+++ rhombus/private/amalgam/match.rkt
@@ -1,22 +1,21 @@
 #lang racket/base
 (require (for-syntax racket/base
-                     racket/list
                      syntax/parse/pre
                      "srcloc.rkt"
-                     "annotation-string.rkt"
-                     "tag.rkt"
-                     "entry-point-adjustment.rkt")
+                     "tag.rkt")
          "expression.rkt"
          "binding.rkt"
          "parse.rkt"
          "else-clause.rkt"
-         (submod "function-parse.rkt" for-build)
          "realm.rkt"
          "parens.rkt"
          (submod "quasiquote.rkt" for-match)
          (only-in "literal.rkt" literal-infoer)
          "if-blocked.rkt"
+         "nested-bindings.rkt"
+         "static-info.rkt"
          "../version-case.rkt")
+
 ;; TEMP approximate `case/equal-always`
 (meta-if-version-at-least
  "8.11.1.8"
@@ -36,26 +35,6 @@
 (define-syntax match
   (expression-transformer
    (lambda (stx)
-     (define ((make-fallback-k else else-parsed else-rhs) val bs b-parseds rhss)
-       (define (make-consts const)
-         (cons const (map (lambda (x) const) bs)))
-       (define (make-consts-stx const)
-         (datum->syntax #f (make-consts const)))
-       (define falses (make-consts #f))
-       (define falses-stx (make-consts-stx #'#f))
-       (define-values (proc arity)
-         (build-case-function no-adjustments '()
-                              #'match
-                              #f #f
-                              (make-consts-stx #'(#f))
-                              #`(#,@(map list bs) (#,else))
-                              #`(#,@(map list b-parseds) (#,else-parsed))
-                              falses-stx falses-stx
-                              falses-stx falses-stx
-                              falses falses
-                              #`(#,@rhss #,else-rhs)
-                              stx))
-       #`(#,proc #,val))
      (syntax-parse stx
        #:datum-literals (group)
        [(form-id in ...+ (alts-tag::alts
@@ -66,20 +45,21 @@
         (values
          (handle-literal-case-dispatch
           stx
-          #'(rhombus-expression (group in ...))
+          #`(rhombus-expression (#,group-tag in ...))
           #'(b ...)
           #'(b.parsed ...)
           #'(clause.rhs ...)
-          ;; fallback-k is called with the remaining non-literal patterns
-          (make-fallback-k #'ignored
-                           (binding-form #'else-infoer #'(#t ignored))
-                           #'(parsed #:rhombus/expr e.parsed)))
+          ;; `fallback-k` is called with the remaining non-literal patterns
+          (lambda (val-id bs b-parseds rhss)
+            (handle-normal-match
+             val-id bs b-parseds rhss
+             #'e.parsed)))
          #'())]
        [(form-id in ...+ (alts-tag::alts
                           clause::pattern-clause
                           ...))
         #:with (b::binding ...) (no-srcloc* #`((#,group-tag clause.bind ...) ...))
-        (define in-expr #'(rhombus-expression (group in ...)))
+        (define in-expr #`(rhombus-expression (#,group-tag in ...)))
         (define b-parseds-stx #'(b.parsed ...))
         (define rhss-stx #'(clause.rhs ...))
         (values
@@ -90,19 +70,17 @@
             (handle-literal-case-dispatch
              stx in-expr
              #'(b ...) b-parseds-stx rhss-stx
-             ;; fallback-k is called with the remaining non-literal patterns
-             (make-fallback-k #'unmatched
-                              (binding-form #'else-infoer #'(#t unmatched))
-                              #`(parsed
-                                 #:rhombus/expr
-                                 (match-fallthrough 'form-id unmatched #,(syntax-srcloc (respan stx))))))))
+             ;; `fallback-k` is called with the remaining non-literal patterns
+             (lambda (val-id bs b-parseds rhss)
+               (handle-normal-match
+                val-id bs b-parseds rhss
+                #`(match-fallthrough 'form-id #,val-id #,(syntax-srcloc (respan stx))))))))
          #'())]
        [(form-id in ...+ (block-tag::block))
         (values
          (relocate+reraw
           (respan stx)
-          #`((match-fallthrough 'form-id (rhombus-expression (group in ...)) #,(syntax-srcloc (respan stx)))
-             (rhombus-expression (group in ...))))
+          #`(match-fallthrough 'form-id (rhombus-expression (#,group-tag in ...)) #,(syntax-srcloc (respan stx))))
          #'())]
        [(form-id in ...+ (_::alts clause ...))
         (for ([c (in-list (syntax->list #'(clause ...)))])
@@ -112,38 +90,69 @@
                                    "expected a pattern followed by a result block"
                                    c)]))]))))
 
+(define-for-syntax (handle-normal-match val-id bs b-parseds rhss else-expr)
+  (for/foldr ([next else-expr])
+             ([b (in-list bs)]
+              [b-parsed (in-list b-parseds)]
+              [rhs (in-list rhss)])
+    (syntax-parse b-parsed
+      [b-parsed::binding-form
+       #:with b-impl::binding-impl #'(b-parsed.infoer-id () b-parsed.data)
+       #:with b-info::binding-info #'b-impl.info
+       ;; use `((lambda ....) ....)` to keep textual order
+       #`((lambda (try-next)
+            (nested-bindings
+             match
+             try-next
+             #f ; failure
+             (#,val-id b-info #,b #f)
+             (begin
+               (b-info.committer-id #,val-id b-info.data)
+               (b-info.binder-id #,val-id b-info.data)
+               (define-static-info-syntax/maybe b-info.bind-id b-info.bind-static-info ...)
+               ...
+               (rhombus-body-expression #,rhs))))
+          (lambda () #,next))])))
+
 (define-for-syntax (handle-literal-case-dispatch stx in-expr
                                                  bs-stx b-parseds-stx rhss-stx
                                                  fallback-k)
+  (define (split-at lst idx)
+    (cond
+      [(eqv? idx 0)
+       (values '() lst)]
+      [else
+       (define-values (l-lst r-lst) (split-at (cdr lst) (sub1 idx)))
+       (values (cons (car lst) l-lst) r-lst)]))
   (define bs (syntax->list bs-stx))
   (define b-parseds (syntax->list b-parseds-stx))
   (define rhss (syntax->list rhss-stx))
+  ;; index at which the initial literal segment ends, if any
   (define maybe-idx
     (for/fold ([maybe-idx #f])
               ([parsed (in-list b-parseds)]
-               [idx (in-naturals 0)])
+               [idx (in-naturals 1)])
       #:break (syntax-parse parsed
                 [b::binding-form
                  (not (free-identifier=? #'b.infoer-id #'literal-infoer))])
-      (add1 idx)))
-  (cond
-    [maybe-idx
-     (define rst-bs (list-tail bs maybe-idx))
-     (define-values (lit-parseds rst-parseds) (split-at b-parseds maybe-idx))
-     (define-values (lit-rhss rst-rhss) (split-at rhss maybe-idx))
-     (relocate+reraw
-      (respan stx)
-      #`(let ([val #,in-expr])
-          (case/equal-always val
-            #,@(for/list ([parsed (in-list lit-parseds)]
-                          [rhs (in-list lit-rhss)])
-                 (syntax-parse parsed
-                   [b::binding-form
-                    #`[b.data (rhombus-body-expression #,rhs)]]))
-            [else #,(fallback-k #'val rst-bs rst-parseds rst-rhss)])))]
-    [else (relocate+reraw
-           (respan stx)
-           (fallback-k in-expr bs b-parseds rhss))]))
+      idx))
+  (relocate+reraw
+   (respan stx)
+   #`(let ([val #,in-expr])
+       #,(cond
+           [maybe-idx
+            (define rst-bs (list-tail bs maybe-idx))
+            (define-values (lit-parseds rst-parseds) (split-at b-parseds maybe-idx))
+            (define-values (lit-rhss rst-rhss) (split-at rhss maybe-idx))
+            #`(case/equal-always val
+                #,@(for/list ([parsed (in-list lit-parseds)]
+                              [rhs (in-list lit-rhss)])
+                     (syntax-parse parsed
+                       [b::binding-form
+                        #`[b.data (rhombus-body-expression #,rhs)]]))
+                [else #,(fallback-k #'val rst-bs rst-parseds rst-rhss)])]
+           [else
+            (fallback-k #'val bs b-parseds rhss)]))))
 
 (struct exn:fail:contract:srcloc exn:fail:contract (srclocs)
   #:property prop:exn:srclocs (lambda (exn) (exn:fail:contract:srcloc-srclocs exn)))
@@ -163,35 +172,6 @@
 
 (define (match-fallthrough who v loc)
   (raise-srcloc-error who v loc))
-
-(define-syntax (else-infoer stx)
-  (syntax-parse stx
-    [(_ static-infos (ok? bind-id))
-     (binding-info annotation-any-string
-                   #'bind-id
-                   #'static-infos
-                   #'()
-                   #'else-matcher
-                   #'else-committer
-                   #'else-binder
-                   #'(ok? bind-id))]))
-
-(define-syntax (else-matcher stx)
-  (syntax-parse stx
-    [(_ arg-id (ok? bind-id) IF success fail)
-     #'(IF ok?
-           success
-           fail)]))
-
-(define-syntax (else-committer stx)
-  (syntax-parse stx
-    [(_ arg-id (ok? bind-id))
-     #'(begin)]))
-
-(define-syntax (else-binder stx)
-  (syntax-parse stx
-    [(_ arg-id (ok? bind-id))
-     #'(define bind-id arg-id)]))
 
 (define-syntax matches
   (expression-infix-operator
