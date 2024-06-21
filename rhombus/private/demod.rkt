cae540244e6c1b20a286471663988e60f3e85185
Wing Hei Chan <whmunkchan@outlook.com>
2024-06-21 23:54:24 +0800

    unbreak compilation on intermediate snapshot versions
    The definition pruning option enabled in af198a4 requires v8.13.0.8,
    so disable the use of `compiler/demod` altogether on intermediate
    v8.13.0.7.

--- rhombus/private/demod.rkt
+++ rhombus/private/demod.rkt
@@ -1,23 +1,22 @@
 #lang racket/base
 (require (for-syntax racket/base
-                     syntax/parse/pre
-                     compiler/cm-accomplice
-                     syntax/modread
                      "version-case.rkt")
-         racket/include
          "version-case.rkt")
 
 (meta-if-version-at-least
- "8.13.0.7"
- (require (submod compiler/demod module-begin))
- (void))
+ "8.13.0.8"
+ (require (for-syntax compiler/cm-accomplice)
+          (submod compiler/demod module-begin))
+ (require (for-syntax syntax/modread
+                      syntax/parse/pre)
+          racket/include))
 
 (provide (rename-out
           [module-begin #%module-begin]))
- 
+
 (define-syntax (module-begin stx)
   (meta-if-version-at-least
-   "8.13.0.7"
+   "8.13.0.8"
    (begin
      ;; Since we expand to a module that no longer has this module as an import,
      ;; we need to register it explicitly for cm:
@@ -30,12 +29,15 @@
          (include-at/relative-to/reader mod-path mod-path mod-path
                                         read-syntax/strip-module))])))
 
-(define-for-syntax (read-syntax/strip-module src in)
-  (define v (with-module-reading-parameterization
-              (lambda ()
-                (read-syntax src in))))
-  (if (eof-object? v)
-      v
-      (syntax-parse  v
-        [(module name lang (mb body ...))
-         #'(begin body ...)])))
+(meta-if-version-at-least
+ "8.13.0.8"
+ (begin)
+ (define-for-syntax (read-syntax/strip-module src in)
+   (define v (with-module-reading-parameterization
+               (lambda ()
+                 (read-syntax src in))))
+   (if (eof-object? v)
+       v
+       (syntax-parse v
+         [(module name lang (mb body ...))
+          #'(begin body ...)]))))
