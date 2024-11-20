0d5bc5f006b0315efaac68a5992b9b794a781e7f
Matthew Flatt <mflatt@racket-lang.org>
2024-11-20 10:12:29 -0700

    correct `head` field of `NameStart` syntax classes
    Also, update the spacer tests for changes in abafcc4924.

--- enforest-lib/enforest/hier-name-parse.rkt
+++ enforest-lib/enforest/hier-name-parse.rkt
@@ -10,7 +10,7 @@
   #:attributes (name head tail)
   #:datum-literals (op)
   #:description "name or dotted name"
-  (pattern (~and stxes (root::name (~and o (op sep)) . _))
+  (pattern (~and stxes (root::name (~and o (op sep)) . orig-tail))
            #:when (eq? name-path-op (syntax-e #'sep))
            #:do [(define head-id (in-name-root-space #'root.name))
                  (define lxc (syntax-local-value* head-id name-root-ref))]
@@ -18,8 +18,9 @@
            #:cut
            #:do [(define-values (head tail) (apply-name-root head-id lxc in-space #'stxes))]
            #:with (~var r (:hier-name-seq in-name-root-space in-space name-path-op name-root-ref)) #`(#,head . #,tail)
+           #:with (next-head . _) #'orig-tail
            #:attr name #'r.name
-           #:attr head #'(root o . r.head)
+           #:attr head #`(root o next-head . #,(cdr (syntax-e #'r.head)))
            #:attr tail #'r.tail)
   (pattern (n::name . tail)
            #:attr head #'(n)
