b553658889c570e6943a3a94698d3f2b426c9277
Sam Phillips <samdphillips@gmail.com>
2025-11-02 21:40:21 -0800

    Repair default `#%effect` not printing multiple values

--- rhombus-lib/rhombus/private/amalgam/effect.rkt
+++ rhombus-lib/rhombus/private/amalgam/effect.rkt
@@ -6,11 +6,15 @@
 
 (provide #%effect)
 
+(define (print-values . vals)
+  (for-each (current-print) vals)
+  (apply values vals))
+
 (define-syntax #%effect
   (expression-transformer
    (lambda (stx)
      (syntax-parse stx
        [(_ . tail)
         #:with e::expression #`(group . tail)
-        (values #'(call-with-values (lambda () e.parsed) (current-print))
+        (values #'(call-with-values (lambda () e.parsed) print-values)
                 #'())]))))
