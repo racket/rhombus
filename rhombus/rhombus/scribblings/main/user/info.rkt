7218ab86aea7cd4c2b834ed7d1af7add090e08af
Matthew Flatt <mflatt@racket-lang.org>
2026-01-22 09:22:59 -0700

    doc: add missing `user-doc-index` entry in "info.rkt"

--- rhombus/rhombus/scribblings/main/user/info.rkt
+++ rhombus/rhombus/scribblings/main/user/info.rkt
@@ -4,3 +4,5 @@
   ;; has 'doc-properties to indicate that this document add or replaces
   ;;  "rhombus/index.html" from "rhombus.scrbl" when rendered into user space:
   '(("user-rhombus.scrbl" (user-doc depends-all no-depend-on) (omit))))
+
+(define user-doc-index "rhombus/scribblings/main/user")
