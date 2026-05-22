cb122b3917050b27659dce0b94a6a98f4cd30f10
Wing Hei Chan <whmunkchan@outlook.com>
2026-05-22 12:14:59 +0800

    correct typos in package description of "rhombus-main"

--- rhombus-main/info.rkt
+++ rhombus-main/info.rkt
@@ -5,7 +5,7 @@
 (define deps '("rhombus-main-distribution"))
 (define implies '("rhombus-main-distribution"))
 
-(define pkg-desc "A shortant packagge for the main Rhombus distribution")
+(define pkg-desc "A shorthand package for the main Rhombus distribution")
 
 (define license '(Apache-2.0 OR MIT))
 
