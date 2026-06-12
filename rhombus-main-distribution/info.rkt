6f81aee76e889f961c52dc05fee4fb675611c566
Matthew Flatt <mflatt@racket-lang.org>
2026-06-12 05:51:11 -0600

    rhombus-main-distribution: add dependencies on doc packages
    Since dependencies of Rhombus are going to be pinned in the Rhombus
    catalog, make sure the pinned set is consistent by dependning on
    "X" when there might otherwise be a dependency just on "X-lib".

--- rhombus-main-distribution/info.rkt
+++ rhombus-main-distribution/info.rkt
@@ -24,7 +24,17 @@
 
     "drracket-core"
     "xrepl"
-    "expeditor"))
+    "expeditor"
+
+    ;; ensure full-package dependency for anythign that
+    ;; might have only a "-lib" dependency
+    "gui-easy"
+    "box-extra"
+    "actor"
+    "http-easy"
+    "resource-pool"
+    "pretty-expressive"
+    "version-case"))
 
 (define pkg-desc "A package that combines all of the packages in the main Rhombus distribution")
 
