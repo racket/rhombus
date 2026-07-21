811b96787c4ecd1201f840b722f19dd1f2c5f489
Wing Hei Chan <whmunkchan@outlook.com>
2026-07-21 08:01:54 +0800

    correct run-time "wrong number of arguments" error messages
    The error rewriting handler was supposed to recognize the
    'racket/primitive message realm, but it recognized name realms
    instead, and the "wrong number of arguments" message only applied to
    errors with the 'racket name realm.

--- rhombus-lib/rhombus/private/amalgam/runtime-config.rkt
+++ rhombus-lib/rhombus/private/amalgam/runtime-config.rkt
@@ -112,7 +112,7 @@
                  [else (values who who-realm)])]
               [else (values who who-realm)]))
           (define-values (new-msg new-msg-realm)
-            (case who-realm
+            (case msg-realm
               [(racket/primitive)
                (define (rhombus s) (values s 'rhombus/primitive))
                (cond
@@ -126,6 +126,10 @@
                  [(regexp-match-positions #rx"^not a procedure;\n expected a procedure that can be applied to arguments" msg)
                   => (lambda (m)
                        (rhombus (string-append "not a function" (substring msg (cdar m)))))]
+                 [(regexp-match-positions #rx"^arity mismatch;\n the expected number of arguments does not match the given number" msg)
+                  => (lambda (m)
+                       (rhombus (string-append "wrong number of arguments in function call"
+                                               (substring msg (cdar m)))))]
                  [(regexp-match-positions
                    #rx"^procedure does not (accept keyword arguments|expect an argument with given keyword)\n  procedure:"
                    msg)
@@ -141,14 +145,6 @@
                  [(regexp-match-positions #rx"^(?:starting |ending |)index is out of range.*?treelist:" msg)
                   => (lambda (m)
                        (rhombus (string-append (regexp-replace* #rx"treelist" (substring msg (caar m) (cdar m)) "list")
-                                               (substring msg (cdar m)))))]
-                 [else (values msg msg-realm)])]
-              [(racket)
-               (define (rhombus s) (values s 'rhombus/primitive))
-               (cond
-                 [(regexp-match-positions #rx"^arity mismatch;\n the expected number of arguments does not match the given number" msg)
-                  => (lambda (m)
-                       (rhombus (string-append "wrong number of arguments in function call"
                                                (substring msg (cdar m)))))]
                  [else (values msg msg-realm)])]
               [else (values msg msg-realm)]))
